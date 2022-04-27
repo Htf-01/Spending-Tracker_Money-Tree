0fbf82119533b0a8b4a2101a5e05e0637c1baf2a
Henry <Henryfrancis89@googlemail.com>
2022-04-27 14:34:47 +0100

    add basic classes to project

---
+++ models/merchant.py
@@ -0,0 +1,8 @@
+class Merchants ():
+    
+    def __init__(self, name, activated = True, filter = True, id)
+        self.name = name
+        self.activated = activated
+        self.filter = filter
+        self.id = id
+    
