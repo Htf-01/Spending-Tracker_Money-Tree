7410ec5eaa19613f4e98987dccc5157dbbd33e8f
Henry <Henryfrancis89@googlemail.com>
2022-04-28 11:45:00 +0100

    Set up Flask and basic HTML boilerplate template

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Import Controllers
+
+
+app = Flask(__name__)
+
+# Import Blueprints
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
