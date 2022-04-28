8b0963dbf5a97a1fbf079d32de18222f430f7374
Henry <Henryfrancis89@googlemail.com>
2022-04-28 11:42:25 +0100

    create budget_repository (see previous commit)

--- repositories/budget_repository.py
+++ repositories/budget_repository.py
@@ -4,34 +4,10 @@ from models.budget import Budget
 
 # CREATE
 ###############################################################
-def save(budget):
-    sql = "INSERT INTO budgets (date) VALUE ( %s) RETURNING id"
-    values = [budget.date]
-    results = run_sql( sql, values )
-    category.id = results[0]['id']
+
 
 # READ
 ###############################################################
-def select_all():
-    categories = []
-
-    sql = "SELECT * FROM categories"
-    results = run_sql(sql)
-
-    for row in results:
-        category = Category(row['name'], row['activated'], row['filtered'], row['id'])
-        categories.append(category)
-    return categories
-
-def select(id):
-    category = None
-    sql = "SELECT * FROM categories WHERE id = %s"
-    values = [id]
-    result = run_sql(sql, values)[0]
-
-    if result is not None:
-        category = Category(result['name'], result['activated'], result['filtered'], result['id'])
-    return category
 
 
 def select_budget(dates):
@@ -49,10 +25,7 @@ def select_budget(dates):
 
 # UPDATE
 ###############################################################
-def update(category):
-    sql = "UPDATE categories SET (name,activated,filtered) = ( %s,%s,%s ) WHERE id = %s"
-    values = [category.name, category.activated, category.filtered, category.id]
-    run_sql(sql, values)
+
     
 # DELETE
 ###############################################################
