3adb9b3de9a7c65ecf418922069e5595f517aeec
Henry <Henryfrancis89@googlemail.com>
2022-04-29 10:41:11 +0100

    add seed data for budget months to the database

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -27,3 +27,66 @@ CREATE TABLE transactions (
   amount INT,
   budget_id INT REFERENCES budgets(id) ON DELETE SET NULL
 );
+
+
+
+Insert into budgets (budget_date) values ('2020-01-01');
+Insert into budgets (budget_date) values ('2020-02-01');
+Insert into budgets (budget_date) values ('2020-03-01');
+Insert into budgets (budget_date) values ('2020-04-01');
+Insert into budgets (budget_date) values ('2020-05-01');
+Insert into budgets (budget_date) values ('2020-06-01');
+Insert into budgets (budget_date) values ('2020-07-01');
+Insert into budgets (budget_date) values ('2020-08-01');
+Insert into budgets (budget_date) values ('2020-09-01');
+Insert into budgets (budget_date) values ('2020-10-01');
+Insert into budgets (budget_date) values ('2020-11-01');
+Insert into budgets (budget_date) values ('2020-12-01');
+Insert into budgets (budget_date) values ('2021-01-01');
+Insert into budgets (budget_date) values ('2021-02-01');
+Insert into budgets (budget_date) values ('2021-03-01');
+Insert into budgets (budget_date) values ('2021-04-01');
+Insert into budgets (budget_date) values ('2021-05-01');
+Insert into budgets (budget_date) values ('2021-06-01');
+Insert into budgets (budget_date) values ('2021-07-01');
+Insert into budgets (budget_date) values ('2021-08-01');
+Insert into budgets (budget_date) values ('2021-09-01');
+Insert into budgets (budget_date) values ('2021-10-01');
+Insert into budgets (budget_date) values ('2021-11-01');
+Insert into budgets (budget_date) values ('2021-12-01');
+Insert into budgets (budget_date) values ('2022-01-01');
+Insert into budgets (budget_date) values ('2022-02-01');
+Insert into budgets (budget_date) values ('2022-03-01');
+Insert into budgets (budget_date) values ('2022-04-01');
+Insert into budgets (budget_date) values ('2022-05-01');
+Insert into budgets (budget_date) values ('2022-06-01');
+Insert into budgets (budget_date) values ('2022-07-01');
+Insert into budgets (budget_date) values ('2022-08-01');
+Insert into budgets (budget_date) values ('2022-09-01');
+Insert into budgets (budget_date) values ('2022-10-01');
+Insert into budgets (budget_date) values ('2022-11-01');
+Insert into budgets (budget_date) values ('2022-12-01');
+Insert into budgets (budget_date) values ('2023-01-01');
+Insert into budgets (budget_date) values ('2023-02-01');
+Insert into budgets (budget_date) values ('2023-03-01');
+Insert into budgets (budget_date) values ('2023-04-01');
+Insert into budgets (budget_date) values ('2023-05-01');
+Insert into budgets (budget_date) values ('2023-06-01');
+Insert into budgets (budget_date) values ('2023-07-01');
+Insert into budgets (budget_date) values ('2023-08-01');
+Insert into budgets (budget_date) values ('2023-09-01');
+Insert into budgets (budget_date) values ('2023-10-01');
+Insert into budgets (budget_date) values ('2023-11-01');
+Insert into budgets (budget_date) values ('2023-12-01');
+Insert into budgets (budget_date) values ('2024-01-01');
+Insert into budgets (budget_date) values ('2024-02-01');
+Insert into budgets (budget_date) values ('2024-03-01');
+Insert into budgets (budget_date) values ('2024-04-01');
+Insert into budgets (budget_date) values ('2024-05-01');
+Insert into budgets (budget_date) values ('2024-06-01');
+Insert into budgets (budget_date) values ('2024-07-01');
+Insert into budgets (budget_date) values ('2024-08-01');
+Insert into budgets (budget_date) values ('2024-09-01');
+Insert into budgets (budget_date) values ('2024-10-01');
+Insert into budgets (budget_date) values ('2024-11-01');
+Insert into budgets (budget_date) values ('2024-12-01');
